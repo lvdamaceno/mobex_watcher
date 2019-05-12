5a4d8b762fd885b7ef8888dd4a30e513dc4423b7
vinicius damaceno <viniciusdamaceno@MacBook-Air-de-vinicius.local>
2019-05-12 10:58:13 -0300

    refatorando o código

--- core.py
+++ core.py
@@ -1,7 +1,5 @@
-'''
-pip install requests
-pip install beautifulsoup4
-'''
+# pip install requests
+# pip install beautifulsoup4
 
 import requests
 from bs4 import BeautifulSoup
@@ -9,32 +7,40 @@ from bs4 import BeautifulSoup
 url = 'http://ciac.ufpa.br/index.php/mobilidade-academica'
 url_base = 'http://ciac.ufpa.br'
 
-# Atribui o resultado da solicitação da página à variável page com o método request.get()
-page = requests.get(url)
-# print(page)
-# print(page.status_code)
-# print(page.text)
 
-# Faz o parse da página e cria um objeto BeatifulSoup como uma estrutura de dados aninhada
-soup = BeautifulSoup(page.text, 'html.parser')
-# Captura todas as instâncias da cat-children
-processes = soup.find_all(class_='cat-children')
-# Captura todas as instâncias da tag ul
-ul = processes[0].find_all('ul')
-# Captura todas as instâncias da tag li
-li = ul[0].find_all('li')
+def capture_page(url):
+    p = requests.get(url)
+    return p
 
-i = 0
-for l in li:
-    # Percorre a lista e printa cada posição formatada para leitura
-    year = li[i].find('a').text
-    articles = li[i].find('dt').text
-    total = li[i].find('dd').text
-    link = li[i].find('a')['href']
 
-    print(year.strip())
-    print(articles.strip(), total.strip())
-    print(url_base + link.strip())
-    print(' ')
+def parse_page():
+    # Faz o parse da página e cria um objeto BeatifulSoup como uma estrutura de dados aninhada
+    soup = BeautifulSoup(capture_page(url).text, 'html.parser')
+    # Captura todas as instâncias da cat-children
+    processes = soup.find_all(class_='cat-children')
+    # Captura todas as instâncias da tag ul
+    ul = processes[0].find_all('ul')
+    # Captura todas as instâncias da tag li
+    li = ul[0].find_all('li')
 
-    i = i + 1
+    return li
+
+
+def return_list():
+    i = 0
+    li = parse_page()
+    for l in li:
+        # Percorre a lista e printa cada posição formatada para leitura
+        year = li[i].find('a').text
+        articles = li[i].find('dt').text
+        total = li[i].find('dd').text
+        link = li[i].find('a')['href']
+
+        print(year.strip())
+        print(articles.strip(), total.strip())
+        print(url_base + link.strip())
+        print(' ')
+
+        i = i + 1
+
+return_list()
